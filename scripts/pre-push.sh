e142b4c306ff61e44421a21be5381b60bf721f95
Pierre Roussel <pierre.roussel1@epsi.fr>
2020-07-23 16:34:49 +0200

    feat precommit

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Running pre-push hook"
-./scripts/run-tests.sh
+echo "Running pre-commit hook"
+./run-tests.bash
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
- then
-Tests must pass before commit!"
+ echo "Tests must pass before commit!"
  exit 1
 fi
