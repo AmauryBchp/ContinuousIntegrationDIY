d2f004da9775cf64875bcd7def8b83f432e269ad
Baboulinux <matteo.dervillers@epsi.fr>
2020-07-23 14:50:18 +0200

    updateTest

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,6 +3,5 @@ cd "${0%/*}/.."
 # let's fake failing test for now 
 echo "Running tests"
 echo "............................" 
-
-#echo "yolo!" && exit 0
-echo "Failed!" && exit 1
+dotnet test
+exit
