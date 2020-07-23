05277fde9b196bbee69952933681906fec3de481
abeauchamp <amaurybchp@gmail.com>
2020-07-23 14:07:41 +0200

    Improve install hook file

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -5,5 +5,6 @@ GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 ln -s ../../scripts/pre-push.sh $GIT_DIR/hooks/pre-push
+ln -s ../../scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
 echo "Done!"
 sleep 5
