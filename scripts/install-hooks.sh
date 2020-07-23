d96d792c8d6293c355ee4a840a9af20620c36db2
Pierre Roussel <pierre.roussel1@epsi.fr>
2020-07-23 16:18:22 +0200

    sur la master

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -5,6 +5,7 @@ GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 ln -s ../../scripts/pre-push.sh $GIT_DIR/hooks/pre-push
-ln -s ../../scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
+ln -s ../../scripts/pre-commit_master.sh $GIT_DIR/hooks/pre-commit_master
+ln -s ../../scripts/pre-commit_prettify.sh $GIT_DIR/hooks/pre-commit_prettify
 echo "Done!"
 sleep 5
