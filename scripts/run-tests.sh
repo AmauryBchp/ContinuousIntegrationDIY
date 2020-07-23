b2a89b418093d28b1cb289bfaa56073d317be497
Pierre Roussel <pierre.roussel1@epsi.fr>
2020-07-23 12:12:31 +0200

    commit -m

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,10 +4,3 @@ cd "${0%/*}/.."
 echo "Running tests"
 echo "............................" 
 echo "Failed!" && exit 1
-
-# example of commands for different languages
-# eslint .         # JS code quality check
-# npm test         # JS unit tests
-# flake8 .         # python code quality check
-# nosetests        # python nose 
-# just put your usual test command here 
